a5effefa91163ff3624c70a5e3502e0d13a2d52b
IvanSerov <VanSer1989@gmail.com>
2020-01-15 19:32:25 +0200

    les3

---
+++ php/les3/config.php
@@ -0,0 +1,15 @@
+<?php 
+// Ваши данные
+$db_server = 'localhost'; //название сервера
+$db_database = 'clients'; //название вашей базы данных
+$db_user = ''; // при необходимости
+$db_password = ''; // пароль если есть
+
+// соединение с базой
+$connection = mysqli_connect ($db_server, $db_database, $db_user, $db_password);
+mysqli_select_db($connection, "clients");
+//Контроль соединения
+if(!$connection) {
+	die("Не удается подключится к базе данных");
+}	
+?>
